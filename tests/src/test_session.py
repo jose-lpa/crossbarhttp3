2d7b2ba189b9ccf884c145d64c5014f87d7428a5
José L. Patiño <jose.lpa@gmail.com>
2016-08-19 16:16:18 +0100

    Fixed `print` statements in `test_session` module.

--- tests/src/test_session.py
+++ tests/src/test_session.py
@@ -1,5 +1,4 @@
 from autobahn.twisted.wamp import ApplicationSession
-from autobahn.wamp import auth
 
 
 class TestSession(ApplicationSession):
@@ -7,19 +6,19 @@ class TestSession(ApplicationSession):
     def onJoin(self, details):
 
         def add_something(x, y, offset=0):
-            print "Add was called"
-            return x+y+offset
+            print('Add was called')
+            return x + y + offset
 
-        self.register(add_something, "test.add")
+        self.register(add_something, 'test.add')
 
         def throw_exception():
-            print "Exception Thrown"
-            raise Exception("Exception Thrown")
+            print('Exception Thrown')
+            raise Exception('Exception Thrown')
 
-        self.register(throw_exception, "test.exception")
+        self.register(throw_exception, 'test.exception')
 
         def subscribe_something(x, y, event=None, **kwargs):
-            print "Publish was called with event %s" % event
+            print('Publish was called with event %s' % event)
 
-        self.subscribe(subscribe_something, "test.publish")
+        self.subscribe(subscribe_something, 'test.publish')
 
