9c1ee727b1dce52c7b5be7b28e84ef468730a971
José L. Patiño <jose.lpa@gmail.com>
2016-08-28 12:19:58 +0100

    Fixed testing compatibility in `setup.py`.

--- setup.py
+++ setup.py
@@ -14,8 +14,17 @@ except ImportError:
 # Define requirements per Python version.
 if sys.version_info >= (3,):
     requirements = []
+    test_requirements = [
+        'autobahn',
+        'twisted'
+    ]
 else:
     requirements = ['future']
+    test_requirements = [
+        'autobahn',
+        'mock',
+        'twisted'
+    ]
 
 
 setup(
@@ -27,13 +36,10 @@ setup(
     license='MIT',
     author_email='eric@thehq.io',
     url='https://github.com/thehq/python-crossbarhttp',
-    keywords=['wamp', 'crossbar', 'websockets'],
+    keywords=['wamp', 'crossbar.io', 'websockets'],
     install_requires=requirements,
     test_suite='tests',
-    tests_require=[
-        'autobahn',
-        'twisted'
-    ],
+    tests_require=test_requirements,
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
@@ -43,6 +49,8 @@ setup(
         'Operating System :: Microsoft :: Windows',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Topic :: Utilities',
     ],
 )
